edf1cfc1b3ca5939c25e276281bfbc7bf376b1b4
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 20:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log("I am ready!");
+const Discord = require('discord.js');const client = new Discord.Client();
+client.on('ready', () => {  
+  console.log('I am ready!');
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("help")) {
-    message.reply("Copy that, Help is on its way ETA:*Crash*....");
+  
+  if (message.content.startsWith("/help")) {
+    message.channel.send("Help is on the way~");
+  } else
+
+  if (message.content.startsWith("/invite")) {
+    message.channel.send("https://discord.gg/v2wwYWE");
   }
 });
 
