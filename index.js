8e321c28be0a673832d8b53c371395f28cc0e924
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-03 08:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');const client = new Discord.Client();
 client.on('ready', () => {  
   console.log('I am ready!');
-
 });
-client.user.setGame('Preparing for breachin!')
+
+
 client.on("message", (message) => {
   
   if (message.content.startsWith("/help")) {
