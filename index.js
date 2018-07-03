288d244d845beeb23893996f10a01f1f9f638f36
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-03 07:04:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const prefix = process.env.BOT_PREFIX
 const client = new Discord.Client();
 client.on('ready', () => {  
   console.log('I am ready!');
@@ -9,44 +10,44 @@ client.on('ready', () => {
     });
 client.on("message", (message) => {
   
-  if (message.content.startsWith("/help")) {
+  if (message.content.startsWith("${prefix}help")) {
     message.channel.send("```Commands:``` /invite, /events");
   }
 
-  if (message.content.startsWith("/invite")) {
+  if (message.content.startsWith("${prefix}invite")) {
     message.channel.send("https://discord.gg/v2wwYWE");
   }
-  if (message.content.startsWith("/events")) {  
+  if (message.content.startsWith("${prefix}events")) {  
     message.channel.send("```Current Events related Misfit Are:``` , 1. Team Extreme's Birthday ");
   }
-   if (message.content.startsWith("/1")) {  
+   if (message.content.startsWith("${prefix}1")) {  
     message.channel.send("Main Topic, DP, Pixel Art Build Battle, Maze, Horse Race, Trivia, Doms trivia, GiveAway, rafle");
   }
-   if (message.content.startsWith("/maintopic")) {  
+   if (message.content.startsWith("${prefix}maintopic")) {  
     message.channel.send("https://forums.teamextrememc.com/index.php/topic/85258-te-7th-birthday/");
   }
-  if (message.content.startsWith("/dp")) {  
+  if (message.content.startsWith("${prefix}dp")) {  
     message.channel.send("No Thread for Drop Party created by event staff yet");
   }
-   if (message.content.startsWith("/pixelartbuildbattle")) {  
+   if (message.content.startsWith("${prefix}pixelartbuildbattle")) {  
     message.channel.send("https://forums.teamextrememc.com/index.php/topic/85350-te-7th-birthday-pixelart-build-battle/");
   }
-   if (message.content.startsWith("/maze")) {  
+   if (message.content.startsWith("${prefix}maze")) {  
     message.channel.send("No thread for maze created by event staff yet");
   }
-  if (message.content.startsWith("/horserace")) {  
+  if (message.content.startsWith("${prefix}horserace")) {  
     message.channel.send("No thread for horserace created by event staff yet");
   }
-  if (message.content.startsWith("/trivia")) {  
+  if (message.content.startsWith("${prefix}trivia")) {  
     message.channel.send("No thread for trivia created by event staff yet");
   }
-  if (message.content.startsWith("/domstrivia")) {  
+  if (message.content.startsWith("${prefix}domstrivia")) {  
     message.channel.send("https://forums.teamextrememc.com/index.php/topic/88868-te-7th-birthday-trivia/");
   }
-  if (message.content.startsWith("/giveaway")) {  
+  if (message.content.startsWith("${prefix}giveaway")) {  
     message.channel.send("https://forums.teamextrememc.com/index.php/topic/87017-te-7th-birthday-giveaway/");
   }
-  if (message.content.startsWith("/rafle")) {  
+  if (message.content.startsWith("${prefix}rafle")) {  
     message.channel.send("https://forums.teamextrememc.com/index.php/topic/86329-te-7th-birthday-raffle/");
   }
 });
